a121465ab6d6c852aadc5ab6480dec4324e73da1
Petar Sambolek <petar.sambolek@inchoo.net>
2014-01-30 15:02:48 +0100

    Init

---
+++ app/code/local/Inchoo/Lenses/Model/Product/Price.php
@@ -0,0 +1,6 @@
+<?php
+
+class Inchoo_Lenses_Model_Product_Price extends Mage_Catalog_Model_Product_Type_Price
+{
+
+}
---
+++ app/code/local/Inchoo/Lenses/Model/Product/Type.php
@@ -0,0 +1,6 @@
+<?php
+
+class Inchoo_Lenses_Model_Product_Type extends Mage_Catalog_Model_Product_Type_Abstract
+{
+	const TYPE_CP_PRODUCT = 'lenses';
+}
