4a6a14dbbf258122427f7afd8f3d2cd56505b7cb
Petar Sambolek <petar.sambolek@inchoo.net>
2014-03-04 15:06:18 +0100

    Working version

---
+++ app/code/local/Inchoo/Onsale/Model/Observer.php
@@ -0,0 +1,18 @@
+<?php
+
+class Inchoo_Onsale_Model_Observer
+{
+    public function getOnSaleCollection($observer)
+    {
+        $param = Mage::app()->getRequest()->getParam('sale');
+
+        $event = $observer->getEvent();
+        $collection = $event->getCollection();
+
+        if(isset($param) && $param==='1'){
+            $collection ->getSelect()
+                        ->where('price_index.final_price < price_index.price');
+        }
+        return $collection;
+    }
+}
