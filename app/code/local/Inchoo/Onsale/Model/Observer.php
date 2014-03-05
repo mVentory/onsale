730f386a64a00b05633c759c26bc89d4394de8ad
Petar Sambolek <petar.sambolek@inchoo.net>
2014-03-05 15:52:37 +0100

    Configurable products on sale

--- app/code/local/Inchoo/Onsale/Model/Observer.php
+++ app/code/local/Inchoo/Onsale/Model/Observer.php
@@ -7,10 +7,11 @@ class Inchoo_Onsale_Model_Observer
         $param = Mage::app()->getRequest()->getParam('sale');
 
         $event = $observer->getEvent();
-        $collection = $event->getCollection()->addFinalPrice();
+        $collection = $event->getCollection();
 
         if(isset($param) && $param==='1'){
-            $collection ->getSelect()
+            $collection ->addFinalPrice()
+                        ->getSelect()
                         ->where('price_index.final_price < price_index.price');
         }
         return $collection;
